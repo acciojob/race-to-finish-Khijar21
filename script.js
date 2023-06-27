c06f6cf96d01c91472fd2a404be3427ec8241b2b
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-06-27 19:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-window.promises = [];
+window.promises = [
+	  new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), getRandomTimeout())),
+	  new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), getRandomTimeout())),
+	  new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), getRandomTimeout())),
+	  new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), getRandomTimeout())),
+	  new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), getRandomTimeout()))
+];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+function htmlelement(elementId,text){
+	const element = document.getElementById("output");
+	if(element){
+		element.innerText = text;
+	}
+}
+
+function getRandomTimeout(){
+	return Math.floor(Math.random() * 5000) + 1000;
+}
+
+Promise.any(promises).then((result) => {
+	htmlelement("output",result);
+})
+
